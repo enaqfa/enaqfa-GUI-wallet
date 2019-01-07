ea03ac0230e81fabcd350c866bf95a36c924978f
andom <46446842+enaqfa@users.noreply.github.com>
2019-01-07 10:45:47 -0500

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "electronicnaqfa")
+set(CN_CURRENCY_DISPLAY_NAME "ElectronicNaqfa")
+set(CN_CURRENCY_TICKER "ElectronicNaqfa")
